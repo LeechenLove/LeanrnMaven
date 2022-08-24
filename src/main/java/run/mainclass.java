9de6fb22c4d1ec3356cc849d6fc36f36489f0ff1
LuLu <727084494@qq.com>
2022-08-24 21:24:24 +0800

    Initial commit

---
+++ src/main/java/run/mainclass.java
@@ -0,0 +1,12 @@
+package run;
+
+/**
+ * @Author: Lulu
+ * @Description: TODO
+ * @DateTime: 2022/8/24 19:41
+ **/
+public class mainclass {
+    public static void main(String[] args) {
+        System.out.println("main class");
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+/**
+ * @Author: Lulu
+ * @Description: TODO
+ * @DateTime: 2022/8/24 18:54
+ **/
+public class test {
+    @Test
+    public void test() {
+        System.out.println("test method");
+    }
+}
